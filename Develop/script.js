f2e60ac1241663423aa8e751de2a608e759c2d3b
Josh Wright <joshwrightaudio@gmail.com>
2021-03-20 12:31:48 -0400

    second commit,variables added

---
+++ Develop/script.js
@@ -0,0 +1,10 @@
+//Event Listener
+document.querySelector("generate").addEventListener("click",writePassword)
+
+//Variables
+var numChars = ["0","1","2","3","4","5","6","7","8","9"];
+var specialChars = ["!","@","#","$","%","^","&","*","<",">","?"]
+var upperChars = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C",
+"V","B","N","M"]
+var lowerChars = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x"
+,"c","v","b","n","m"]
